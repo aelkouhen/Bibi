8a77d6dbc544144fb23e124a748787dee07e1d09
aelkouhen <Arigatou86*>
2019-03-17 23:14:43 +0100

    Adding lyne and mima routes

--- src/main/java/com/carhub/api/ProxyApplication.java
+++ src/main/java/com/carhub/api/ProxyApplication.java
@@ -1,13 +1,22 @@
-package com.carhub.api.proxy;
+package com.carhub.api;
 
+import com.carhub.api.proxy.filter.AuthHeaderFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
+@EnableEurekaClient
 @SpringBootApplication
 public class ProxyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProxyApplication.class, args);
+	}
+
+	@Bean
+	AuthHeaderFilter authHeaderFilter() {
+		return new AuthHeaderFilter();
 	}
 }
---
+++ src/main/java/com/carhub/api/proxy/config/ProxyConfig.java
@@ -0,0 +1,40 @@
+package com.carhub.api.proxy.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+
+import javax.servlet.http.HttpServletResponse;
+
+@Configuration
+@EnableResourceServer
+public class ProxyConfig extends ResourceServerConfigurerAdapter {
+
+    @Value("${security.oauth2.resource.id}")
+    private String resourceId;
+
+    @Override
+    public void configure(final HttpSecurity http) throws Exception {
+        http.csrf().disable()
+        // make sure we use stateless session; session won't be used to store user's state.
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and()
+        // handle an authorized attempts
+        .exceptionHandling().authenticationEntryPoint((req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED))
+        .and()
+        // authorization requests config
+        .authorizeRequests()
+        .antMatchers("/**")
+        .permitAll();
+    }
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources
+                .resourceId(resourceId);
+    }
+}
