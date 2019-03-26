de291289e9cbe5a4d9280a5d3ad2fe63b6b44dc8
aelkouhen <Arigatou86*2>
2019-03-26 16:14:49 +0100

    Adding routes to Rana & Mino Apis

--- src/main/java/com/carhub/api/proxy/config/DocumentationController.java
+++ src/main/java/com/carhub/api/proxy/config/DocumentationController.java
@@ -19,6 +19,9 @@ public class DocumentationController implements SwaggerResourcesProvider {
         List<SwaggerResource> resources = new ArrayList<>();
         resources.add(swaggerResource("Lyne API - Vehicle foundation service", "/auto/v2/api-docs", "1.0"));
         resources.add(swaggerResource("Mima API - Authentication / Authorization service", "/auth/v2/api-docs", "1.0"));
+        resources.add(swaggerResource("Mino API - Media management service", "/media/v2/api-docs", "1.0"));
+        resources.add(swaggerResource("Rana API - Social activities service", "/social/v2/api-docs", "1.0"));
+
         return resources;
     }
 
